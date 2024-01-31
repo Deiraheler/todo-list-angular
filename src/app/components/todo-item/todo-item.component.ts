c559df6a5a73d8944fe98444caa5d797019074a3
Deiraheler <53556704+Deiraheler@users.noreply.github.com>
2024-01-31 10:41:45 +0000

    Filter added

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {NgClass, NgForOf} from "@angular/common";
+import { TodoItem } from '../../shared/todo-item';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgClass
+  ],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.css'
 })
 export class TodoItemComponent {
+  @Input() item: TodoItem = new TodoItem("");
+  @Output() statusChange = new EventEmitter<TodoItem>();
 
+  onStatusChange() {
+    this.statusChange.emit(this.item);
+  }
 }
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,33 +1,69 @@
 import { Component } from '@angular/core';
 import { TodoItem } from "../../shared/todo-item";
 import { FormsModule } from "@angular/forms";
-import {NgForOf} from "@angular/common";
+import {JsonPipe, NgForOf} from "@angular/common";
+import {TodoItemComponent} from "../todo-item/todo-item.component";
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
   imports: [
     FormsModule,
-    NgForOf
+    NgForOf,
+    TodoItemComponent,
+    JsonPipe
   ],
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
+
 export class TodoListComponent {
   inputData: string = "";
-  ToDoList: TodoItem[] = [
-    new TodoItem("Lol"),
-    new TodoItem("Lolik")
-  ];
+  _ToDoList: TodoItem[] = [];
+  private _filterInput: string = "0";
+  get filterInput(): string {
+    return this._filterInput;
+  }
+  set filterInput(value: string) {
+    this._filterInput = value;
+    console.log('Selected option changed:', value);
+  }
+  filterOptions: any[] = [
+    {
+      value: 0,
+      text: "All"
+    },
+    {
+      value: 1,
+      text: "Finished"
+    },
+    {
+      value: 2,
+      text: "Uncompleted"
+    },
+  ]
+
+  get ToDoList(): TodoItem[]{
+    if (this.filterInput === '0') {
+      return this._ToDoList;
+    } else {
+      const filterStatus = this.filterInput === '1';
+      return this._ToDoList.filter(item => item.status === filterStatus);
+    }
+  }
+
+  set ToDoList(todoItem: TodoItem){
+    this._ToDoList.push(todoItem);
+  }
 
   createToDo(){
     if (this.inputData.trim()) {
-      this.ToDoList.push(new TodoItem(this.inputData));
+      this.ToDoList = new TodoItem(this.inputData);
       this.inputData = "";
     }
   }
 
-  onStatusChange(item: TodoItem){
+  onStatusChange(item: TodoItem) {
     item.status = !item.status;
   }
 }
